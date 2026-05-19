4cbe898e8ff03ae4ad5fc4e3f8524af0ec7e4285
CLopezLara <1i.lopez.lara.carlos@gmail.com>
2026-05-18 19:33:22 -0600

    feat: add Link to to header brand

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,48 @@
-import "./Header.css"
-import logo from "../../images/to-do-list-logo.png"
-import { NavLink } from "react-router"
-import { useState } from "react"
+import "./Header.css";
+import logo from "../../images/to-do-list-logo.png";
+import { Link, NavLink } from "react-router";
+import { useState } from "react";
 function Header() {
-    const [menu , setMenu] = useState(false)
-    const openMenu = () =>{
-        setMenu((state) => !state);
-    }
+  const [menu, setMenu] = useState(false);
+  const openMenu = () => {
+    setMenu((state) => !state);
+  };
   return (
     <section className="header-container">
-        <img className= "header-logo" src={logo} alt="to do list logo"/>
-        <button className="hamburguer-button" onClick={openMenu}>{menu ? "X":"☰"}</button>
-        <nav className={ `navbar-container ${menu ? "open" : ""}`}>
-            <ul className= "navbar-list">
-                <li className="navbar-option"><NavLink to="/" className="nav-link" onClick={openMenu}>Inicio</NavLink></li>
-                <li className="navbar-option"><NavLink to="/tareas" className="nav-link" onClick={openMenu}>Tareas</NavLink></li>
-                <li className="navbar-option"><NavLink to="/tablero" className="nav-link" onClick={openMenu}>Tablero</NavLink></li>
-                <li className="navbar-option"><NavLink to="/login" className="nav-link" onClick={openMenu}>Iniciar sesión</NavLink></li>
-            </ul>
-        </nav>
+      <Link to="/" className="header-brand">
+        <img className="header-logo" src={logo} alt="to do list logo" />
+        <h1 className="header-title">ToDoList</h1>
+      </Link>
+
+      <button className="hamburguer-button" onClick={openMenu}>
+        {menu ? "X" : "☰"}
+      </button>
+      <nav className={`navbar-container ${menu ? "open" : ""}`}>
+        <ul className="navbar-list">
+          <li className="navbar-option">
+            <NavLink to="/" className="nav-link" onClick={openMenu}>
+              Inicio
+            </NavLink>
+          </li>
+          <li className="navbar-option">
+            <NavLink to="/tareas" className="nav-link" onClick={openMenu}>
+              Tareas
+            </NavLink>
+          </li>
+          <li className="navbar-option">
+            <NavLink to="/tablero" className="nav-link" onClick={openMenu}>
+              Tablero
+            </NavLink>
+          </li>
+          <li className="navbar-option">
+            <NavLink to="/login" className="nav-link" onClick={openMenu}>
+              Iniciar sesión
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
